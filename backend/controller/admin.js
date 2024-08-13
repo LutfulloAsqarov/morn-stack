0b8c841cdd800ac5d9f9710fd34896bbeca55274
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-08-14 00:47:02 +0500

    backend

---
+++ backend/controller/admin.js
@@ -0,0 +1,243 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Admins, validateAdmin } from "../models/adminSchema.js";
+
+class AdminsController {
+    async get(req, res) {
+        try {
+            const admins = await Admins.find();
+
+            if (!admins) {
+                return res.status(400).json({
+                    msg: "Admin is not defined",
+                    variant: "error",
+                    payload: null,
+                });
+            }
+            res.status(200).json({
+                msg: "All Admins",
+                variant: "success",
+                payload: admins,
+            });
+        } catch {
+            res.status(500).json({
+                msg: "Server error",
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async getProfile(req, res) {
+        try {
+            let admin = await Admins.findById(req.admin._id);
+            res.status(200).json({
+                msg: "admin registered successfully",
+                variant: "success",
+                payload: admin,
+            });
+        } catch {
+            res.status(500).json({
+                msg: err.message,
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async updateProfile(req, res) {
+        try {
+            const id = req.admin._id;
+            const { username } = req.body;
+
+            const admin = await Admins.findById(id);
+            if (!admin) {
+                return res.status(404).json({
+                    msg: "Foydalanuvchi topilmadi",
+                    variant: "error",
+                    payload: null,
+                });
+            }
+
+            const checkUsername = await Admins.findOne({ username });
+            if (checkUsername && checkUsername._id.toString() !== id) {
+                return res.status(400).json({
+                    msg: "Bu username mavjud",
+                    variant: "warning",
+                    payload: null,
+                });
+            }
+
+            const updateAdmin = await Admins.findByIdAndUpdate(
+                id,
+                { ...req.body, password: admin.password },
+                { new: true }
+            );
+
+            return res.status(200).json({
+                msg: "Profil yangilandi",
+                variant: "success",
+                payload: updateAdmin,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                msg: "Server xatosi",
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async getAdmin(req, res) {
+        try {
+            const { id } = req.params;
+            let admin = await Admins.findById(id);
+            res.status(200).json({
+                msg: "admin registered successfully",
+                variant: "success",
+                payload: admin,
+            });
+        } catch {
+            res.status(500).json({
+                msg: err.message,
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async registerAdmin(req, res) {
+        try {
+            const { error } = validateAdmin(req.body);
+            if (error)
+                return res.status(400).json({
+                    msg: error.details[0].message,
+                    variant: "error",
+                    payload: null,
+                });
+
+            const { username, password } = req.body;
+
+            const existingAdmin = await Admins.findOne({ username });
+
+            if (existingAdmin)
+                return res.status(400).json({
+                    msg: "Admin already exists.",
+                    variant: "error",
+                    payload: null,
+                });
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const admin = await Admins.create({
+                ...req.body,
+                password: hashedPassword,
+            });
+
+            res.status(201).json({
+                msg: "Admin registered successfully",
+                variant: "success",
+                payload: admin,
+            });
+        } catch (err) {
+            res.status(500).json({
+                msg: err.message,
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async loginAdmin(req, res) {
+        const { username, password } = req.body;
+
+        const admin = await Admins.findOne({ username });
+        if (!admin)
+            return res.status(400).json({
+                msg: "Invalid username or password.",
+                variant: "error",
+                payload: null,
+            });
+
+        const validPassword = await bcrypt.compare(password, admin.password);
+        if (!validPassword)
+            return res.status(400).json({
+                msg: "Invalid username or password.",
+                variant: "error",
+                payload: null,
+            });
+
+        const token = jwt.sign(
+            { _id: admin._id, role: admin.role },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "24h",
+            }
+        );
+
+        res.status(200).json({
+            msg: "Logged in successfully",
+            variant: "success",
+            payload: { token, admin: admin },
+        });
+    }
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+            await Admins.findByIdAndDelete(id);
+            res.status(201).json({
+                msg: "Admin is deleted",
+                variant: "success",
+                payload: null,
+            });
+        } catch {
+            res.status(500).json({
+                msg: "server error",
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+
+    async updateAdmin(req, res) {
+        try {
+            const { id } = req.params;
+
+            const existingAdmin = await Admins.findOne({
+                username: req.body.username,
+            });
+            if (existingAdmin && id !== existingAdmin._id?.toString()) {
+                return res.status(400).json({
+                    msg: "Admin already exists.",
+                    variant: "error",
+                    payload: null,
+                });
+            }
+
+            if (existingAdmin) {
+                req.body.password = existingAdmin.password;
+            }
+
+            let admin = await Admins.findByIdAndUpdate(id, req.body, {
+                new: true,
+            });
+            res.status(200).json({
+                msg: "Admin updated",
+                variant: "success",
+                payload: admin,
+            });
+        } catch (err) {
+            res.status(500).json({
+                msg: err.message,
+                variant: "error",
+                payload: null,
+            });
+        }
+    }
+}
+
+export default new AdminsController();
+
+// export default new AdminController();
---
+++ backend/routes/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+// import AdminController from "../controller/admin.js";
+import AdminsController from "../controller/admin.js";
+import category from "../controller/category.js";
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/api/admins", AdminsController.get);
+router.post("/api/admins/sign-up", AdminsController.registerAdmin);
+router.post("/api/admins/sign-in", AdminsController.loginAdmin);
+router.get("/api/profile", [auth], AdminsController.getProfile);
+router.delete("/api/delete/admins/:id", AdminsController.delete);
+router.patch("/api/update/admins/:id", AdminsController.updateAdmin);
+router.get("/api/admins/:id", AdminsController.getAdmin);
+router.patch("/api/update/profile", [auth], AdminsController.updateProfile);
+
+router.get("/api/categories", category.get);
+router.post("/api/categories", [auth], category.create);
+router.delete("/api/categories/:id", category.delete);
+router.patch("/api/categories/:id", category.update);
+
+export default router;
